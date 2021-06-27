7fca2f43ddd065e753f93df6b9ea95ca8dee1dce
dinushchathurya <dinushchathurya21@gmail.com>
2021-06-27 09:10:59 +0530

    :sparkles: create function to show user create blade

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,4 +13,10 @@ class UserController extends Controller
         return view('users.index',compact('data'))
             ->with('i', ($request->input('page', 1) - 1) * 5);
     }
+
+    public function create()
+    {
+        $roles = Role::pluck('name','name')->all();
+        return view('users.create',compact('roles'));
+    }
 }
