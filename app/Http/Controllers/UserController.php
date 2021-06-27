74183859acb8fc16d746797555baab29f702e9d4
dinushchathurya <dinushchathurya21@gmail.com>
2021-06-27 09:01:24 +0530

    :sparkles: create function to get all users to user index blade file

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class UserController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $data = User::orderBy('id','DESC')->paginate(5);
+        return view('users.index',compact('data'))
+            ->with('i', ($request->input('page', 1) - 1) * 5);
+    }
 }
