9237f4843ffaea91d7827299e468f12ad00e4e7a
dinushchathurya <dinushchathurya21@gmail.com>
2021-06-27 09:20:11 +0530

    :sparkles: peering required facdes to UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use Spatie\Permission\Models\Role;
+use DB;
+use Hash;
+use Illuminate\Support\Arr;
 
 class UserController extends Controller
 {
